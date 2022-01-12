799c31909245fd9f359edd66e279fa83d2741297
Di <dimeng@ebay.com>
2022-01-12 17:56:31 +0800

    first commit

---
+++ precache-manifest.6b735924ae3e2e73b920d8852a2e5c88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c252ed8b4114c673fd867fad5aa245b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84bb04793292f4dde263",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "78014453022117be401a",
+    "url": "/static/css/main.aeab76ed.chunk.css"
+  },
+  {
+    "revision": "84bb04793292f4dde263",
+    "url": "/static/js/2.245ace9b.chunk.js"
+  },
+  {
+    "revision": "8f8195584b7d67df6900",
+    "url": "/static/js/3.c8305c87.chunk.js"
+  },
+  {
+    "revision": "78014453022117be401a",
+    "url": "/static/js/main.462fd126.chunk.js"
+  },
+  {
+    "revision": "ec35a75380b1bafd0923",
+    "url": "/static/js/runtime~main.cfb8a917.js"
+  }
+]);
